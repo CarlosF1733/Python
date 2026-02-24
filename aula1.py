1b8780a6e151eef942492b4cf64dfd04d4b87db0
CarlosF1733 <carlosfonseca1733@gmail.com>
2026-02-24 21:43:39 +0000

    Nova raiz limpa - Aulas

---
+++ aula1.py
@@ -0,0 +1,8 @@
+# ask the name and the age and return the name and the age in ten years
+
+nome = input("name: ")
+idade = input("age: ")
+dezanos = int(idade) + 10
+
+print(f"Qual o teu nome? {nome}")
+print(f"A tua idade é {idade}, daqui a 10 anos tens {dezanos}")
